841bc87b2a097f7d963e5f1de97914db7c186b7e
Hyerim926 <ekfrhsk96@icloud.com>
2022-11-10 14:00:53 +0900

    feat: Question 2 done

--- section6/2.js
+++ section6/2.js
@@ -4,31 +4,20 @@
 * 입력된 문자열에서 소괄호 ( ) 사이에 존재하는 모든 문자를 제거하고 남은 문자만 출력하는 프로그램을 작성하세요.
 */
 
-//[
-//   '(', 'A', '(', 'B', 'C', ')',
-//   'D', ')', 'E', 'F', '(', 'G',
-//   '(', 'H', ')', '(', 'I', 'J',
-//   ')', 'K', ')', 'L', 'M', '(',
-//   'N', ')'
-// ]
-
 const solution = (str) => {
     let strToArr = str.split('');
 
-    console.log(strToArr);
-
-
-    let answer = '';
+    const stack = [];
 
     for (let i = 0; i < strToArr.length; i++) {
-        let isString = strToArr[i].match(/[a-zA-Z]/);
-        if (isString) {
-
+        if (strToArr[i] === '(' || strToArr[i].match(/[a-zA-Z]/)) {
+            stack.push(strToArr[i]);
+        } else if (strToArr[i] === ')') {
+            stack.splice(stack.lastIndexOf('('));
         }
-
     }
 
-    return answer;
+    return stack.join('');
 }
 
 console.log(solution('(A(BC)D)EF(G(H)(IJ)K)LM(N)'));
